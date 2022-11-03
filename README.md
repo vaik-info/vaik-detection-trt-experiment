68ee3362af6a01a5abd9c1f51bbc58d11e1dd032
vaik-info <info@vaikobo.com>
2022-11-03 11:34:26 +0900

    first commit

--- README.md
+++ README.md
@@ -4,9 +4,38 @@ Create Pascal VOC xml file by inference model. Calc mAP and draw a box with scor
 
 ## Example
 
+![vaik-detection-trt-experiment](https://user-images.githubusercontent.com/116471878/199637208-76753193-a391-4b5b-a84f-5418502a8d2a.png)
+
 
 ## Install
+
+- amd64(g4dn.xlarge)
 
 ```shell
 pip install -r requirements.txt
 ```
+
+- arm64(JetsonXavierNX)
+
+```shell
+cat requirements.txt | xargs -n 1 pip install
+```
+
+## Usage
+
+### Create Pascal VOC xml file
+
+```shell
+python inference.py --input_saved_model_path '~/output_trt_model/model.fp16.trt' \
+                --input_classes_path '~/.vaik-mnist-detection-dataset/classes.txt' \
+                --input_image_dir_path '~/.vaik-mnist-detection-dataset/valid' \
+                --output_xml_dir_path '~/.vaik-mnist-detection-dataset/valid_inference' \
+                --score_th 0.2 \
+                --nms_th 0.5
+```
+
+#### Output
+
+![vaik-detection-trt-experiment-output](https://user-images.githubusercontent.com/116471878/199637324-dae09efc-abb1-4c76-ba6d-e7fe1846bd22.png)
+
+-----
---
+++ inference.py
@@ -0,0 +1,51 @@
+import argparse
+import os
+import glob
+import numpy as np
+from PIL import Image
+from tqdm import tqdm
+from vaik_detection_trt_inference.trt_model import TrtModel
+from vaik_pascal_voc_rw_ex import pascal_voc_rw_ex
+
+
+def main(input_saved_model_path, input_classes_path, input_image_dir_path, output_xml_dir_path, score_th, nms_th):
+    os.makedirs(output_xml_dir_path, exist_ok=True)
+    classes = []
+    with open(input_classes_path) as f:
+        for line in f:
+            classes.append(line.strip())
+    model = TrtModel(input_saved_model_path, tuple(classes))
+
+    types = ('*.jpg', '*.jpeg', '*.png', '*.JPG', '*.JPEG', '*.PNG')
+    image_path_list = []
+    for files in types:
+        image_path_list.extend(glob.glob(os.path.join(input_image_dir_path, files), recursive=True))
+
+    image_list = []
+    for image_path in tqdm(image_path_list):
+        image = np.asarray(Image.open(image_path).convert('RGB'))
+        image_list.append(image)
+    objects_dict_list, raw_pred = model.inference(image_list, score_th=score_th, nms_th=nms_th)
+
+    for image_path, objects_dict_list in zip(image_path_list, objects_dict_list):
+        output_xml_path = os.path.join(output_xml_dir_path, os.path.splitext(os.path.basename(image_path))[0] + '.xml')
+        pascal_voc_rw_ex.write_pascal_voc_xml_dict(output_xml_path, image_path,
+                                                   object_extend_dict_list=objects_dict_list)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='inference')
+    parser.add_argument('--input_saved_model_path', type=str, default='~/output_trt_model/model.fp16.trt')
+    parser.add_argument('--input_classes_path', type=str, default='~/.vaik-mnist-detection-dataset/classes.txt')
+    parser.add_argument('--input_image_dir_path', type=str, default='~/.vaik-mnist-detection-dataset/valid')
+    parser.add_argument('--output_xml_dir_path', type=str, default='~/.vaik-mnist-detection-dataset/valid_inference')
+    parser.add_argument('--score_th', type=float, default=0.2)
+    parser.add_argument('--nms_th', type=float, default=0.5)
+    args = parser.parse_args()
+
+    args.input_saved_model_path = os.path.expanduser(args.input_saved_model_path)
+    args.input_classes_path = os.path.expanduser(args.input_classes_path)
+    args.input_image_dir_path = os.path.expanduser(args.input_image_dir_path)
+    args.output_xml_dir_path = os.path.expanduser(args.output_xml_dir_path)
+
+    main(**args.__dict__)
